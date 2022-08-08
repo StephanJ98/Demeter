f991faf365932133b9e347ff36f2864ef291a4b6
StephanJ98 <diego.jeandon@gmail.com>
2022-08-08 18:34:06 +0200

    :rocket: Change from Redux to Recoil.

--- frontend/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-import userManagement from '../features/usersManagement/userManagement'
-
-export default configureStore({
-  reducer: {
-    users: userManagement
-  },
-})
