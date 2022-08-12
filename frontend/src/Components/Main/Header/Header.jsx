2ef276c5a710827603de9bc4ddb88e13f6d13ec9
StephanJ98 <diego.jeandon@gmail.com>
2022-08-12 15:29:18 +0200

    :pencil: Modification of the movile header aspect

--- frontend/src/Components/Main/Header/Header.jsx
+++ frontend/src/Components/Main/Header/Header.jsx
@@ -1,18 +1,60 @@
 import React, { useState } from 'react'
 import styles from './Header.module.css'
-import Search from '../../../Icons/Search.svg'
+import ReactModal from 'react-modal'
+
 import AddTask from './AddTask/AddTask'
 import ImportTask from './ImportTask/ImportTask'
 import LogOut from './LogOut/LogOut'
+import { ReactComponent as Close } from '../../../Icons/Close.svg'
+import { ReactComponent as Options } from '../../../Icons/Options.svg'
+import { ReactComponent as Search } from '../../../Icons/Search.svg'
 
 export default function Header() {
   const [search, setSearch] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
 
   const handleSearch = (event) => {
     event.preventDefault()
 
     //TODO
     console.log(search)
+    alert('ToDo')
+  }
+
+  let options = () => {
+
+    if (window.screen.width > 600) {
+      return (
+        <div id={styles.lowerRow}>
+          <AddTask />
+          <ImportTask />
+          <LogOut />
+        </div>
+      )
+    } else {
+      return (
+        <>
+          <button id={styles.optionsBtn} onClick={() => setIsOpen(true)}>
+            <Options alt="Options Button" />
+          </button>
+
+          <ReactModal
+            isOpen={isOpen}
+            appElement={document.body}
+            className={styles.optionsModal}
+            overlayClassName={styles.overlayOptionsModal}
+          >
+            <button id={styles.modalBtn} onClick={() => setIsOpen(false)}>
+              <Close alt="Close Button" />
+            </button>
+
+            <AddTask />
+            <ImportTask />
+            <LogOut />
+          </ReactModal>
+        </>
+      )
+    }
   }
 
   return (
@@ -26,15 +68,11 @@ export default function Header() {
             value={search}
             onChange={(event) => setSearch(event.target.value)} />
           <button id={styles.searchIcon} type="submit" onClick={(e) => handleSearch(e)}>
-            <img src={Search} alt="Busqueda" />
+            <Search alt="Search" />
           </button>
         </form>
       </div>
-      <div id={styles.lowerRow}>
-        <AddTask />
-        <ImportTask />
-        <LogOut />
-      </div>
+      {options()}
     </div>
   )
 }
