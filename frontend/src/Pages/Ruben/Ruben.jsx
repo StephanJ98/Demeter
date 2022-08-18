8018e3b9e9cec6b673fee029baa2dbfd4ad44b34
StephanJ98 <diego.jeandon@gmail.com>
2022-08-18 11:26:50 +0200

    :pencil: Desafio ruben

--- frontend/src/Pages/Ruben/Ruben.jsx
+++ frontend/src/Pages/Ruben/Ruben.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export default function Ruben() {
-  return (
-    <div>aaaaaaaaaaaaaa</div>
-  )
+    return (
+        <div>
+            <p>- El desafio consiste en tener un div que ocupe el 50% de la pantalla de alto y ancho.</p>
+            <p>- Que este div este centrado en el centro de la pantalla y que tenga un fondo de color distinto a blanco.</p>
+            <p>- Dentro de este div habra un un boton, un campo de texto.</p>
+            <p>- En interno tendras una variable que este inicializada a 1.</p>
+            <p>- Cada vez que pulses el boton la variable se incrementara de 1.</p>
+            <p>- Dentro del boton debes mostrar el valor de la variable.</p>
+            <p>- Y dentro del campo de texto deves mostrar el valor de la variables x2.</p>
+        </div>
+    )
 }
