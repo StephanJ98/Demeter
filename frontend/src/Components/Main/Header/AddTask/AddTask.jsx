b5baf4a26098210f3a50e6eb579ebcf964b127c5
StephanJ98 <diego.jeandon@gmail.com>
2022-09-08 18:00:46 +0200

    :bug: Correction of CSS

--- frontend/src/Components/Main/Header/AddTask/AddTask.jsx
+++ frontend/src/Components/Main/Header/AddTask/AddTask.jsx
@@ -5,6 +5,61 @@ import Close from '../../../../Icons/Close.svg'
 
 export default function AddTask() {
     const [isOpen, setIsOpen] = useState(false)
+    const [modalMode, setModalMode] = useState('normal')
+
+    const handleModalCloseReturn = () => {
+        if (modalMode === 'weekly' || modalMode === 'unique') {
+            setModalMode('normal')
+        } else {
+            setIsOpen(false)
+        }
+    }
+
+    const ModalCloseBtn = () => {
+        return (
+            <button id={styles.modalBtn} onClick={() => handleModalCloseReturn()}>
+                <img src={Close} alt="Close Button" />
+            </button>
+        )
+    }
+
+    const NormalModal = () => {
+        if (modalMode === 'normal') {
+            return (
+                <>
+                    <ModalCloseBtn />
+
+                    <section id={styles.modalSection}>
+                        <h2 id={styles.modalSectionText}>Add a weekly task or a unique task?</h2>
+                        <section id={styles.modalSectionBtn}>
+                            <button className={styles.modalBtns} onClick={() => setModalMode('weekly')}>Weekly</button>
+                            <button className={styles.modalBtns} onClick={() => setModalMode('unique')}>Unique</button>
+                        </section>
+                    </section>
+                </>
+            )
+        } else if (modalMode === 'weekly') {
+            return (
+                <>
+                    <ModalCloseBtn />
+
+                    <section id={styles.modalSection}>
+                        <h2 id={styles.modalSectionText}>Weekly</h2>
+                    </section>
+                </>
+            )
+        } else if (modalMode === 'unique') {
+            return (
+                <>
+                    <ModalCloseBtn />
+
+                    <section id={styles.modalSection}>
+                        <h2 id={styles.modalSectionText}>Unique</h2>
+                    </section>
+                </>
+            )
+        }
+    }
 
     return (
         <div id={styles.container}>
@@ -18,13 +73,7 @@ export default function AddTask() {
                 overlayClassName={styles.overlayOptionsModal}
                 className={styles.taskModal}
             >
-                <button id={styles.modalBtn} onClick={() => setIsOpen(false)}>
-                    <img src={Close} alt="Close Button" />
-                </button>
-
-                <section id={styles.modalSection}>
-                    aaa
-                </section>
+                {NormalModal()}
             </ReactModal>
         </div>
     )
