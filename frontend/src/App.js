dcb32fafe5c3d0aa67351ec7df65c4ac9585bbe9
StephanJ98 <diego.jeandon@gmail.com>
2022-09-12 17:56:14 +0200

    :pencil: Ruben

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './Pages/Login/Login';
-import Register from './Pages/Register/Register';
-import Main from './Pages/Main/Main';
-import NoValidRoute from './Pages/NoValidRoute/NoValidRoute';
 import Ruben from './Pages/Ruben/Ruben';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        {/* To remove */} <Route path="/ruben" element={<Ruben />} />
-        <Route path="*" element={<NoValidRoute />} />
+        <Route path="/" element={<Ruben />} />
       </Routes>
     </Router>
   );
